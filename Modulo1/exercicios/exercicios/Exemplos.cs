e8331cc42cd6fb399cd12924fb5da55d54467108
unknown <Administrador@DD01.texs.server>
2019-09-28 14:31:43 -0300

    Treinamento1

---
+++ Modulo1/exercicios/exercicios/Exemplos.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicios
+{
+    public class Exemplos
+    {
+        public void FatorialNumeroQuatro()
+        {
+            int fatorial = 4*3*2*1;
+            Console.WriteLine($"Fatorial quatro: 4 x 3 x 2 x 1 = {fatorial}");
+        }
+
+        public int FatorialNumeroCinco()
+        {
+            return 5*4*3*2*1;
+        }
+        public int FatorialNumero(int valor)
+        {
+            //int cont = valor;
+            int resultado = valor;
+            for (int i = valor - 1; i > 0; i--)
+            {
+                resultado = resultado * i;
+            }
+            return resultado;
+        }
+        public void MaiorValor(int Valor1, int Valor2)
+        {
+            if (Valor1 > Valor2)
+            {
+                Console.WriteLine($"O maior valor é: {Valor1}");
+            }
+            else if (Valor1 < Valor2)
+            {
+                Console.WriteLine($"O maior valor é: {Valor2}");
+            }
+            else
+            {
+                Console.WriteLine("Os Valores são iguais");
+            }
+        }
+        public void ParImpar(int Numero) 
+        {
+            decimal Valor;
+            Valor = Numero % 2;
+            if (Valor == 0)
+            {
+                Console.WriteLine("O numero é Par");
+            }
+            else
+            {
+                Console.WriteLine("O número é Impar");
+            }
+        }
+        public decimal Calculadora(decimal Valor1, decimal Valor2, char operador)
+        {
+            decimal Resultado;
+            if (operador == '+')
+            {
+                Resultado = Valor1 + Valor2;
+            }
+            else if (operador == '-')
+            {
+                Resultado = Valor1 - Valor2;
+            }
+            else if (operador == '/')
+            {
+                Resultado = Valor1 / Valor2;
+            }
+            else
+            {
+                Resultado = Valor1 * Valor2;    
+            }
+            return Resultado;
+        }
+
+        public void Tabuada(int Valor)
+        {
+            int Resultado;
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine($"{Valor} X {i} = {Valor * i}");
+            }
+        }
+        public decimal Conversao(decimal Valor, char Tipo)
+        {
+            switch (Tipo)
+            {
+                case 'C':
+                    return ((Valor * 9) / 5) + 32;
+                case 'F':
+                    return ((Valor - 32) * 5) / 9;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ Modulo1/exercicios/exercicios/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World1!");
+            Console.WriteLine("Hello World2!");
+
+
+            string nome;
+            bool inativo;
+            int numero;
+            long numero_1;
+            decimal valor = 10.52842M;
+            DateTime hoje = DateTime.Now;
+            string hoje_Formatado = hoje.ToString("dd/MM/yyyy");
+            DateTime data = new DateTime(2019, 09, 01);
+            char tipo;
+
+            int[] array_int = new int[5];
+            string[] array_string = new string[5];
+
+            array_string[0] = "A";
+            array_string[1] = "B";
+            array_string[2] = "C";
+            array_string[3] = "D";
+            array_string[4] = "E";
+
+            Console.WriteLine(">>" + hoje_Formatado);
+            Console.WriteLine($">> {hoje_Formatado}");
+            Console.WriteLine($">> {valor.ToString("#0.00")}");
+
+            /*Console.Write("Digite deu nome: ");
+            nome = Console.ReadLine();*/
+            
+            //if (nome != "BRUNO")
+            /*if (nome.ToUpper() == "BRUNO")
+            {
+                Console.WriteLine("Nome é igual a BRUNO");
+            }
+            else
+            {
+                Console.Write("Nome é diferente de BRUNO");
+                Console.WriteLine($"O nome é igual a: {nome.PadRight(10,'-')}");
+            }*/
+
+            Exemplos exemplo = new Exemplos();
+            exemplo.FatorialNumeroQuatro();
+            Console.WriteLine($"O fatorial de 5 é: {exemplo.FatorialNumeroCinco()}");
+
+            int nFatorial;
+            Console.Write("Digite um numero: ");
+            nFatorial = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine($"O fatorial de {nFatorial} é: {exemplo.FatorialNumero(nFatorial)}");
+
+            int Primeiro = Convert.ToInt32(Console.ReadLine());
+            int Segundo  = Convert.ToInt32(Console.ReadLine());
+            exemplo.MaiorValor(Primeiro, Segundo);
+
+            exemplo.Tabuada(5);
+        }
+    }
+}
